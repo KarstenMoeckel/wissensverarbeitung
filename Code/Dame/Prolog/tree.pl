ee3e65d7e4cb9094d75ec20239e269f6293725c3
karsten <karsten.moeckel212222@gmail.com>
2015-12-10 10:46:54 +0100

    Tiefensuche an Änderungen angepasse

--- Code/Dame/Prolog/tree.pl
+++ Code/Dame/Prolog/tree.pl
@@ -12,11 +12,8 @@ getEnemy(Color, Enemy) :-
 % Player: current player.
 % Depth: Depth of the search.
 createTree(Player, Depth) :-
-    getEnemy(Player, Enemy),
     createStoneList(World),
     assertz(node(_, 0, World, Player, [])),
-
-    % Start the depth search.
     depthSearch(Depth, World, Player, node(_, 0, World, Player, [])).
 
 % Check if depth is reached.
@@ -25,10 +22,12 @@ createTree(Player, Depth) :-
 %     call depthSearch_1
 depthSearch(Depth, World, Player, ParentNode) :-
     writeln('Tiefensuche, Tiefe: ' + Depth),
-    Depth > 0,
+    Depth >= 0,
     NewDepth is Depth - 1,
-    maplist(depthSearch(World, Player, NewDepth, ParentNode),
-                CurrentPlayerStones).
+    %% subtract only the players stones
+    getEnemy(Player, Enemy),
+    subtract(World, [stone(_,Enemy,_)], PlayerStones),
+    maplist(depthSearch(World, Player, NewDepth, ParentNode), PlayerStones).
 
 % For each Possible Move
 %     do the move and call depthSearch with the new list of stones.
@@ -36,64 +35,33 @@ depthSearch(Depth, World, Player, ParentNode) :-
 % For each Possible Attack
 %     do the attack and call depthSearch with the new list of stones.
 depthSearch(World, Player, Depth, ParentNode, Stone) :-
-    member(Stone, CurrentPlayerStones),
-    getEnemy(Player, Enemy),
+    member(Stone, World),
+    writeln(Stone),
     (
-        %% check if stone can move
-        moveDirections(Stone, topLeft),
-        (
-            validMove(Stone, topLeft, Player, World),
-            doMove(Stone, World, topLeft, NewList),
-            assertz(node(ParentNode, 0, World, Player, [])),
-            depthSearch(Depth, World, Enemy, node(ParentNode, 0, NewList, Player, [])),
-            fail
-        )
-        ;
-        moveDirections(Stone, topRight),
-        (
-            validMove(Stone, topRight, Player, World),
-            doMove(Stone, World, topRight, NewList),
-            assertz(node(ParentNode, 0, World, Player, [])),
-            depthSearch(Depth, World, Enemy, node(ParentNode, 0, NewList, Player, [])),
-            fail
-        )
-        ;
-        moveDirections(Stone, bottomLeft),
-        (
-            validMove(Stone, bottomLeft, Player, World),
-            doMove(Stone, World, bottomLeft, NewList),
-            assertz(node(ParentNode, 0, World, Player, [])),
-            depthSearch(Depth, World, Enemy, node(ParentNode, 0, NewList, Player, [])),
-            fail
-        )
-        ;
-        moveDirections(Stone, bottomRight),
-        (
-            validMove(Stone, bottomRight, Player, World),
-            doMove(Stone, World, bottomRight, NewList),
-            assertz(node(ParentNode, 0, World, Player, [])),
-            depthSearch(Depth, World, Enemy, node(ParentNode, 0, NewList, Player, [])),
-            fail
-        )
-        %% Check if stone can hit
+        findall(X, moveDirections(Stone, X), Relations),
+        maplist(depthSearch(World, Player, Depth, ParentNode, Stone), Relations)
     ).
+
+depthSearch(World, Player, Depth, ParentNode, Stone, Relation) :-
+    writeln('stage 3: ' + Relation),
+    validMove(Stone, Relation, Player, World),
+    doMove(Stone, World, Relation, NewList),
+    assertz(node(ParentNode, 0, World, Player, [])),
+    writeln('Move ' + Relation),
+    getEnemy(Player, Enemy).
+    %depthSearch(Depth, World, Enemy, node(ParentNode, 0, NewList, Player, [])).
 
 %% subtract removes occurence of stone from StoneList and writes the rest of
 %% the list into NewList.
 %% Add new Stone to NewList.
 doMove(Stone, StoneList, Direction, NewList) :-
-   Stone = stone(Field,Color,Type),
+    Stone = stone(Field,Color,Type),
     subtract(StoneList, [Stone], TempList),
     hasRelation(Field,Field2,Direction),
-    con(TempList, [stone(Field2, Color, Type)], NewList).
+    append(TempList, [stone(Field2, Color, Type)], NewList).
 
 addChildNode(Node, NewChildnode) :-
    Node = node(Parent, Value,World,Player,ChildNodes),
    retract(Node),
-   con(ChildNodes, [NewChildnode], NewList),
+   append(ChildNodes, [NewChildnode], NewList),
    assertz(node(Parent, Value, World, Player, NewList)).
-
-%% Concatenate two lists.
-con([],L1,L1).
-con([X|Tail],L2,[X|Tail1]):-
-    con(Tail,L2,Tail1).
