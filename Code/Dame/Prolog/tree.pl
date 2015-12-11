8700150e3a69dc7aae630cfffff2c5d9ab9c5aff
karsten <karsten.moeckel212222@gmail.com>
2015-12-11 10:53:35 +0100

    daily commit

--- Code/Dame/Prolog/tree.pl
+++ Code/Dame/Prolog/tree.pl
@@ -14,15 +14,17 @@ getEnemy(Color, Enemy) :-
 createTree(Player, Depth) :-
     createStoneList(World),
     assertz(node(_, 0, World, Player, [])),
-    depthSearch(Depth, World, Player, node(_, 0, World, Player, [])).
+    depthSearch(Depth, node(_, 0, World, Player, [])).
 
 % Check if depth is reached.
 %
 % For each stone of the current Player
 %     call depthSearch_1
-depthSearch(Depth, World, Player, ParentNode) :-
-    writeln('Tiefensuche, Tiefe: ' + Depth),
-    Depth >= 0,
+depthSearch(Depth, ParentNode) :-
+    writeln('Stage 1: ' + Depth),
+    arg(3, ParentNode, World),
+    arg(4, ParentNode, Player),
+    Depth > 0,
     NewDepth is Depth - 1,
     %% subtract only the players stones
     getEnemy(Player, Enemy),
@@ -38,18 +40,23 @@ depthSearch(World, Player, Depth, ParentNode, Stone) :-
     member(Stone, World),
     writeln(Stone),
     (
+        %% Get all possible Moves
         findall(X, moveDirections(Stone, X), Relations),
-        maplist(depthSearch(World, Player, Depth, ParentNode, Stone), Relations)
+        maplist(depthSearch(World, Player, Depth, ParentNode, Stone), Relations),
+
+        %% Get all possible HitChances
+
+        %% Get all leafes of the tree and start a new search
+        findall(node(Parent, Value, World, Player, []),
+                node(Parent, Value, World, Player, []), Nodes),
+        maplist(depthSearch(Depth), Nodes)
     ).
 
 depthSearch(World, Player, Depth, ParentNode, Stone, Relation) :-
-    writeln('stage 3: ' + Relation),
     validMove(Stone, Relation, Player, World),
     doMove(Stone, World, Relation, NewList),
-    assertz(node(ParentNode, 0, World, Player, [])),
-    writeln('Move ' + Relation),
-    getEnemy(Player, Enemy).
-    %depthSearch(Depth, World, Enemy, node(ParentNode, 0, NewList, Player, [])).
+    writeln(World + " ||| " + NewList),
+    assertz(node(ParentNode, 0, NewList, Player, [])).
 
 %% subtract removes occurence of stone from StoneList and writes the rest of
 %% the list into NewList.
